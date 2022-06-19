de3ad6857847acc60a401d57363c8ed70a42253e
nardiyansah <janatri1398@gmail.com>
2022-06-19 13:46:11 +0700

    guess number higher or lower

---
+++ Go/guess_number_higher_or_lower.go
@@ -0,0 +1,26 @@
+package main
+
+func guessNumber(n int) int {
+	lower, upper := 0, n
+
+	if guess(lower) == 0 {
+		return lower
+	}
+
+	if guess(upper) == 0 {
+		return upper
+	}
+
+	for {
+		guessedNumber := (lower + upper) / 2
+		resultGuess := guess(guessedNumber)
+		if resultGuess == 0 {
+			return guessedNumber
+		} else if resultGuess == -1 {
+			upper = guessedNumber
+		} else {
+			lower = guessedNumber
+		}
+
+	}
+}
