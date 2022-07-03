25b92914e08ea762ae557cea350a1652c97ca341
nardiyansah <janatri1398@gmail.com>
2022-07-03 16:02:56 +0700

    find the difference

---
+++ Go/find_the_difference.go
@@ -0,0 +1,25 @@
+package main
+
+func findTheDifference(s string, t string) byte {
+	mapS := make(map[rune]int)
+	mapT := make(map[rune]int)
+
+	for _, v := range s {
+		mapS[v] += 1
+	}
+
+	for _, v := range t {
+		mapT[v] += 1
+	}
+
+	var c byte
+
+	for k, _ := range mapT {
+		if mapS[k] != mapT[k] {
+			c = byte(k)
+			break
+		}
+	}
+
+	return c
+}
