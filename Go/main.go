ba33d170f88a30812902003e84651d79720aa6b0
nardiyansah <janatri1398@gmail.com>
2022-08-07 07:18:03 +0700

    third max

--- Go/main.go
+++ Go/main.go
@@ -1,14 +1,32 @@
 package main
 
+import (
+	"sort"
+)
+
 func thirdMax(nums []int) int {
-	if len(nums) < 3 {
+	sort.Ints(nums)
+
+	// remove duplicate
+	unique := make(map[int]bool)
+	newNums := make([]int, 0)
+	for _, v := range nums {
+		if unique[v] {
+			continue
+		}
+		newNums = append(newNums, v)
+		unique[v] = true
+	}
+
+	if len(newNums) < 3 {
 		temp := 0
-		for _, v := range nums {
+		for _, v := range newNums {
 			if v > temp {
 				temp = v
 			}
 		}
 		return temp
 	}
-	return 0
+
+	return newNums[len(newNums)-3]
 }
--- Go/main_test.go
+++ Go/main_test.go
@@ -15,6 +15,14 @@ func TestThirdMax(t *testing.T) {
 			input:    []int{1, 2},
 			expected: 2,
 		},
+		{
+			input:    []int{3, 2, 1},
+			expected: 1,
+		},
+		{
+			input:    []int{2, 2, 3, 1},
+			expected: 1,
+		},
 	}
 
 	for _, test := range tests {
