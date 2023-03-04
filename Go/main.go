cd9486485ba6103e1fbb0f3e28b80ff2d4756834
nardiyansah <janatri1398@gmail.com>
2023-03-05 06:39:52 +0700

    robot return to origin

--- Go/main.go
+++ Go/main.go
@@ -1448,3 +1448,24 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	return result
 }
+
+func judgeCircle(moves string) bool {
+	// if U then y + 1
+	// if D then y - 1
+	// if R then x + 1
+	// if L then x - 1
+	x, y := 0, 0
+	for _, char := range moves {
+		if char == 'U' {
+			y += 1
+		} else if char == 'D' {
+			y -= 1
+		} else if char == 'R' {
+			x += 1
+		} else if char == 'L' {
+			x -= 1
+		}
+	}
+
+	return x == 0 && y == 0
+}
